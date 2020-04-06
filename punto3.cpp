759e1a0922266d8ed2a65fcaaa6c941a75622c8e
RoblesJP <juan.robles1417@gmail.com>
2020-04-06 00:10:11 -0300

    punto3a agregado

--- punto3.cpp
+++ punto3.cpp
@@ -7,6 +7,7 @@
 // funciones
 void cargarMatriz(int, int, int*);
 void mostrarMatriz(int, int, int*);
+void paresPorFila(int, int, int*);
 
 int main() {
     srand(time(NULL));
@@ -16,6 +17,8 @@ int main() {
     cargarMatriz(N, M, matriz[0]);
     printf("- Matriz cargada: \n");
     mostrarMatriz(N, M, matriz[0]);
+    printf("- Mostrando numeros pares por fila");
+    paresPorFila(N, M, matriz[0]);
     return 0;
 }
 
@@ -36,4 +39,17 @@ void mostrarMatriz(int filas, int columnas, int* matriz) {
         printf("\n");
     }
     printf("\n");
+}
+
+void paresPorFila(int filas, int columnas, int* matriz) {
+    int cont;
+    for (int i = 0; i < filas; i++) {
+        cont = 0;
+        for(int j = 0; j < columnas; j++) {
+            if (*(matriz + (i * columnas + j)) % 2 == 0) {
+                cont++;
+            }
+        }
+        printf("\nNumeros pares en la fila %d: %d", i + 1, cont);
+    }
 }
