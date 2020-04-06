92a604603b9d92dc2ad13854105ea8762b6891bf
RoblesJP <juan.robles1417@gmail.com>
2020-04-06 18:37:44 -0300

    arreglo dinamico agregado

--- tp2_4.cpp
+++ tp2_4.cpp
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct {
-    double ghz;
+    int ghz;
     int anio_fabricacion;
     char* tipo_procesador;
     int cant_nucleos;
 } PC;
 
-
 int main() {
+    srand(time(NULL));
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+    int cant;
+    printf("\n>> Ingrese la cantidad de PCs: "); scanf("%d", &cant);
+    PC lista_pc[cant];
+    printf("- Lista de %d PCs generada.", cant);
 
+    printf("\n\n");
     return 0;
 }
