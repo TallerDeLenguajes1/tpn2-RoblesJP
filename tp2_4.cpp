72e36fbb5aac3980239073954c50f626823f17c3
RoblesJP <juan.robles1417@gmail.com>
2020-04-06 17:53:34 -0300

    estuctura de una PC y arreglo de tipos de procesador agregados

---
+++ tp2_4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    double ghz;
+    int anio_fabricacion;
+    char* tipo_procesador;
+    int cant_nucleos;
+} PC;
+
+
+int main() {
+    char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+    return 0;
+}
